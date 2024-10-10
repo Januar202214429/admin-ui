819273038583f47ba264658eb3c063a2d18306a1
Helmi azkia <azkiasekian@gmail.com>
2024-10-10 11:57:30 +0700

    mengubah replikasi

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h3>Januar Rizky Adsyamsa hallo</h3>
+      <h3>kontil</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
